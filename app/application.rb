6de38fc45e215d52d2afbee37cdf13cf204c5b96
David McGrann <dpmcgrann@gmail.com>
2018-07-22 20:52:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       @@cart.collect do |c|
-        resp.write "#{ }\n"
+        resp.write "#{c}\n"
       end
     else
       resp.write "Path Not Found"
